6c66c2fdd74ada72a805963f5c5e3258a0395bbf
cortajarena <inigo@cortajarena.com>
2022-12-28 19:58:16 +0100

    Cleanup

--- main.py
+++ main.py
@@ -4,26 +4,11 @@ from selenium.webdriver.common.keys import Keys
 import time
 import pandas as pd
 
-from custom_logger import set_logger
-
 
 LOGGER = set_logger("selenium_stats")
 
 
-def get_death_from_record(record: str) -> str:
-    """In the records, the second word implies the total number of
-    deaths in a given day.
-    """
-    try:
-        return record.split(" ")[1]
-    except:
-        return None
-
-
-def set_chrome_options() -> None:
-    """Sets chrome options for Selenium.
-    Chrome options for headless browser is enabled.
-    """
+def get_chrome_options() -> None:
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
@@ -34,27 +19,13 @@ def set_chrome_options() -> None:
     return chrome_options
 
 
-def take_death_number(date_str: str,
-                      chrome_options: Options,
-                      url: str) -> str:
-    """Main function of the scraper.
-    Takes the death numbers from the given webpage for
-    official statistics.
-    The web site might change in time. Hence, the operations applied in sequence:
-    1. Find the element by ID in the given webpage with 'tarih'.
-    2. Enter the date as text in the given element ID.
-    3. Gets the text from the class element 'tablePagination'"""
+def main(chrome_options: Options) -> None:
     try:
         driver = webdriver.Chrome(options=chrome_options)
         driver.set_window_size(800, 600)
-        driver.get(url)
+        driver.get('https://www.ciudadela.eu/')
         time.sleep(5)
-        date_element = driver.find_element_by_id("tarih")
-        date_element.send_keys(date_str)
-        date_element.send_keys(Keys.ENTER)
-        time.sleep(5)
-        pagination = driver.find_element_by_class_name("tablePagination")
-        return pagination.text
+        return None
     except Exception as shit:
         LOGGER.error(f"{shit} happened.")
         return None
